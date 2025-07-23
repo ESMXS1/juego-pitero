50de761b5f2525e33409e460048c9f66db407cf4
Moises santos <161497430+ESMXS1@users.noreply.github.com>
2025-07-22 20:39:36 -0700

    Create juego_todo_feo.py

---
+++ juego-pitero/juego_todo_feo.py
@@ -0,0 +1,6 @@
+print('Bienvenido al juego todo feo!')
+for i in range(3):
+    nombre = input('Como te llamas? ')
+    print('Hola', nombre, 'juega mejor la proxima vez jajaja')
+    if i == 2:
+        print('Hace un dia hermoso alla fuera las aves cantan las flores florecen pero.... ni;os como tu deberian arder en el infierno') 
