892853d7b0b44c137e78707eaecee7e1adb087c9
Moises santos <161497430+ESMXS1@users.noreply.github.com>
2025-07-22 20:41:20 -0700

    actualizacion-no es actualizacion es inicio del maldito codigo >:c
    00001000111100110

--- juego-pitero/juego_todo_feo.py
+++ juego-pitero/juego_todo_feo.py
@@ -3,4 +3,4 @@ for i in range(3):
     nombre = input('Como te llamas? ')
     print('Hola', nombre, 'juega mejor la proxima vez jajaja')
     if i == 2:
-        print('Hace un dia hermoso alla fuera las aves cantan las flores florecen pero.... ni;os como tu deberian arder en el infierno') 
+        print('Hace un dia hermoso alla fuera las aves cantan las flores florecen pero.... ni;os como tu deberian arder en el infierno') s
